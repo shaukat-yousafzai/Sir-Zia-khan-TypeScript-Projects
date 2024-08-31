43271076b7f6dfe740aa61a9c06809e7543e685f
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-08-31 18:02:02 +0000

    update

---
+++ QuestionNo15/main.ts
@@ -0,0 +1,33 @@
+//Question 
+
+//  Changing Guest List: You just heard that one of your guests can’t make the
+//  dinner, so you need to send out a new set of invitations. You’ll have to think of
+//  someone else to invite.
+// • Start with your program from Exercise 14. Add a print statement at the
+// end of your program stating the name of the guest who can’t make it.
+
+// • Modify your list, replacing the name of the guest who can’t make it with
+// the name of the new person you are inviting.
+
+// • Print a second set of invitation messages, one for each person who is still
+// in your list.
+// Answer 
+
+//  Guest List
+
+let guestList:string[] = ["yousafzai","khan","shaukat"]
+
+// You just heard that one of your guests can’t make the dinner
+
+let notAvialbleGuest:string = "yousafzai"
+console.log(notAvialbleGuest, "can't make the dinner")
+
+// new Guest 
+let newGuest:string = "aziz";
+// replacing or changing guest list
+//  you need to send out a new set of invitations
+guestList[guestList.indexOf(notAvialbleGuest)] = newGuest
+guestList.map((guest)=>{
+    console.log("Dear",guest+", you are invited")
+})
+export {}
