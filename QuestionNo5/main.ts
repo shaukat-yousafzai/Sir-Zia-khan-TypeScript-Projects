18117706c22b6928dab82e158a967efa5981d73a
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-08-31 14:30:52 +0000

    update

---
+++ QuestionNo5/main.ts
@@ -0,0 +1,19 @@
+// Question
+
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new 
+//variable called message. Print your message.
+
+
+// Answer
+//  store the famous person’s name in a variable called famous_person
+let famous_person:string = 'Albert Einstein';
+
+// Then compose your message and store it in a new 
+// variable called message 
+
+let message:string = '“A person who never made a mistake never tried anything new.”'
+
+//  Repeat Exercise 4
+//  Print your message
+
+console.log(famous_person, "once said,", message )
