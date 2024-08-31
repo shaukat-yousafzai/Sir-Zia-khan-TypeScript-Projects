0497ea1881a1cf0edc1b07ff123e347ce4b50a0a
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-08-31 15:21:11 +0000

    update

---
+++ QuestionNo10/main.ts
@@ -0,0 +1,17 @@
+// Question 
+
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point,
+//  just add your name and the current date at the top of each program file.
+//  Then write one sentence describing what the program does.
+
+// Answer
+
+// Author Name     :  Shaukat Aziz
+// Today Date      :  31/Aug/2024
+// Program Details :  This is too much simple addition program
+
+let a:number = 4
+let b:number = 5
+let result:number = a + b;
+console.log(result)
