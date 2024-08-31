d9b3a332c207c90adb280f109c0f130c9ccbd108
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-08-31 15:20:30 +0000

    update

---
+++ QuestionNo7/main.ts
@@ -0,0 +1,15 @@
+// Question
+ 
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+
+// Answer
+
+// addition operations that each result in the number 8. 
+console.log(5+3)
+// subtraction operations that each result in the number 8. 
+console.log(12-4)
+// multiplication operations that each result in the number 8. 
+console.log(4*2)
+// division operations that each result in the number 8. 
+console.log(16/2)
