320e6d5e6e58864f843c7421332a4c3edf2e6f93
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-08-31 15:21:22 +0000

    update

---
+++ QuestionNo11/main.ts
@@ -0,0 +1,15 @@
+// Question 
+
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+// Answer
+
+// Store the names of a few of your friends in a array called names
+
+let names:string[] = ["Shaukat","Aziz","Yousafzai","Khan"];
+
+// Print each person’s name by accessing each element in the list. one at a time.
+
+for (const name of names) {
+    console.log(name)
+}
