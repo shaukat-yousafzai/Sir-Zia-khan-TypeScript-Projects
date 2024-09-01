456e699cc550793b79a696cacf6db2ecdaa284ab
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-09-01 10:29:06 +0000

    update

---
+++ QuestionNo34/main.ts
@@ -0,0 +1,19 @@
+// Question
+
+//// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+
+// Answer 
+
+//Think of at least three kinds of your favorite pizza. Store these pizza names in a array
+let Pizzas : string[] = ['Fajita' , 'BBQ chiken' , 'Cuban'];
+
+// use a for loop to print the name of each pizza
+for( let Pizza of Pizzas){
+
+    console.log(`I like ${Pizza} Pizza.`);
+}
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza.
+ console.log(`I really love pizza!`);
