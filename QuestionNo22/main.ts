b8a13c17f81bbd489943639ba6c222cf6a256a66
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-09-01 08:11:15 +0000

    update

---
+++ QuestionNo22/main.ts
@@ -0,0 +1,23 @@
+// Question 
+
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
+// to produce an index error. Make sure you correct the error before closing the program.
+
+// Answer
+
+
+// If you haven’t received an array index error in one of your programs yet, try to make one happen
+
+// creating array
+let newArray : string[] = ['Ahmed' , 'Bilal' , 'mujtaba'];
+
+// accessing array by index
+console.log(newArray[4]);
+
+// correct the error
+console.log(newArray);
+
+// Add index 4
+ 
+newArray[4] = "correct error"
+console.log(newArray[4]);
