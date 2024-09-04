74e940ce7c7c069ec038fbf885da85494c768f24
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-09-04 15:50:40 +0000

    update

---
+++ NODE_PROJECTS/Project04_calculator/main.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+let ans = await inquirer.prompt([
+    {
+        name:"num1",
+        type:"number",
+        message:"Please enter your first number"
+     },
+     {
+        name:"num2",
+        type:"number",
+        message:"Please enter your second number"
+     },
+     {
+        name:"operators",
+        type:"list",
+        message:"Please Select the operator",
+        choices:["+","-","*","/"]
+     }
+])
+// varible declarations
+let number1:number = ans.num1
+let number2:number = ans.num2
+let opt:string = ans.operators
+// conditional statements
+if (opt == "+"){
+    console.log(number1,opt,number2,"=",number1 + number2)
+}else
+if (opt == "-"){
+    console.log(number1,opt,number2,"=",number1 - number2)
+}else
+if (opt == "*"){
+    console.log(number1,opt,number2,"=",number1 * number2)
+}else
+if (opt == "+"){
+    console.log(number1,opt,number2,"=",number1 / number2)
+}else
+{
+    console.log("Invalid option")
+}
