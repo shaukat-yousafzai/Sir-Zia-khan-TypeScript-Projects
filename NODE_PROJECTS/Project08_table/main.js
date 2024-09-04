9a778906908f296e09abae1a0e160ef2075e7d09
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-09-04 12:14:35 +0000

    update

---
+++ NODE_PROJECTS/Project08_table/main.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+// user input
+let userInput = await inquirer.prompt({
+    name: "num1",
+    type: "number",
+    message: "Please enter your number",
+    validate: (input) => {
+        const num2 = parseInt(input);
+        if (isNaN(num2)) {
+            return "please enter the number";
+        }
+        else {
+            return true;
+        }
+    }
+});
+// usr input value
+let userNum = userInput.num1;
+// logic for table
+for (let i = 1; i < 12; i++) {
+    console.log(`${userNum} * ${i} = ${userNum * i}`);
+}
