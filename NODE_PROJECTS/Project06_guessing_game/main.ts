7053fb54e36944b200d425ff77a9e28e9fadc16c
Shaukat Khan <162915389+shaukat-yousafzai@users.noreply.github.com>
2024-09-04 07:25:25 +0000

    update

---
+++ NODE_PROJECTS/Project06_guessing_game/main.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+const secretNumber:number = Math.floor(Math.random()*2) + 1
+
+console.log(chalk.bold.underline("i have picked a number"))
+
+let userInp = await inquirer.prompt({
+    type: "list",
+    name: "guess",
+    message: "Guess the number",
+    choices: ["1", "2"]
+})
+
+// converting user input number
+
+let nam1:number = parseInt(userInp.guess)
+
+switch(true){
+    case nam1 === secretNumber:
+        console.log(chalk.green.bold("Congratulations! You guessed correctly"))
+        break
+    default:
+        console.log(chalk.red.bold("Sorry! You guessed wrong. The correct number is: ", secretNumber))
+        break
+}
